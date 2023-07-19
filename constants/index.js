a6c35ab470c03320dfaf42ab3a9e92668261960b
Mrfti <79114164+Mrfti@users.noreply.github.com>
2023-07-19 16:38:14 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x48806024efeba2569dc6b7b0816fb99670df4c3fd226ba6c5e6affcbb3107b94";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
